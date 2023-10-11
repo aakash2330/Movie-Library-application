ece8e29a061719c3b1ee9de64c7f9b5f7a052579
aakash2330 <aakash2330@gmail.com>
2023-10-12 03:51:40 +0530

    form-added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -15,8 +16,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <html lang="en" suppressHydrationWarning>
+    <head />
+    <body>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </body>
+  </html>
   )
 }
---
+++ src/types/formTypes.ts
@@ -0,0 +1,47 @@
+
+import { type } from 'os'
+import {coerce, z} from 'zod'
+
+
+
+
+export const formSchema = z.object({
+    movieName: z.string().min(2,"Must be at least 2 characters.",).max(1000 , "Must not be more than 1000 characters"),
+    duration:z.string().refine(
+        (duration) => {try{
+
+            const durationArr = duration.split(""); //extracting wheter the entered duration is m or h
+
+            if(durationArr[durationArr.length-1]=="m"){
+                    if(+duration.split("m")[0]>=1 && +duration.split("m")[0]<=720 ) return true; 
+            }
+            if(durationArr[durationArr.length-1]=="h"){
+                    if(+duration.split("h")[0]>=0.1 && +duration.split("h")[0]<=12)return true;
+            }
+            else return false
+
+        }catch(error){
+            return false
+        }},
+        { message: "Duration must be 1-720m or 0.1-12h in format: Xh or Xm" }
+      )
+,
+    rating: z.string().refine((rating)=>{
+        try{
+            if(+rating>=1 && +rating<=10)return true;
+            return false
+        }
+        catch(error){
+            return error;
+        }
+    }, { message: "Rating should be between 1 and 10" })
+})
+
+export type formType = z.infer<typeof formSchema>
+
+export type formAttributesType = {
+    name:"movieName" | "duration" | "rating"
+    label:string,
+    placeholder:string,
+    description:string
+}
