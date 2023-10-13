29506d1f80ce25506748c93a82b254515535fc14
aakash2330 <aakash2330@gmail.com>
2023-10-13 14:51:27 +0530

    icon-direction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className='h-screen flex flex-col items-center justify-center text-[#c0fb50] gap-[3rem]'>
     <h1 >WELCOME TO MOVIE LIBRARY</h1>
-    <p className='flex items-center justify-center gap-[0.5rem]'>Click on<Image className='w-[1.5rem]' src={menuIcon} alt="SideMenuOpener" priority={true}/>for menu </p>
+    <p className='flex items-center justify-center gap-[0.5rem]'>Click on <Image className='w-[1.5rem]' src={menuIcon} alt="SideMenuOpener" priority={true}/> {"for menu (bottom left)"} </p>
     </div>
     
   )
